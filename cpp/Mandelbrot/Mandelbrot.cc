7b4a30a3bcda613b1afc2870159ff550cd3cadb8
Tomas Wolf <tomasw@synopsys.com>
2021-04-15 15:30:51 -0400

    Implements threading to calculate intermediate steps, master joins at the end

--- cpp/Mandelbrot/Mandelbrot.cc
+++ cpp/Mandelbrot/Mandelbrot.cc
@@ -9,12 +9,17 @@
 #include <vector>
 #include <tuple>
 #include <random>
+#include <thread>
 
-// Its just easier to split this if we're working with power of 2's
-#define THREAD_AMOUNT 8
-#define WIDTH 1024
-#define HEIGHT 1024
-#define ITER_THRESHOLD 34
+int thread_number = 64;
+// It's easier to not check border conditions and just create sizes of 2 to some power
+int base_pixel_number = 2048*2*2 / (thread_number*2);
+int width = base_pixel_number * thread_number;
+int height = base_pixel_number * thread_number;
+double x_base_translation = 1.5;
+double y_base_translation = 0.5;
+int iter_threshold = 50;
+
 
 using namespace std;
 using namespace std::chrono;
@@ -28,27 +33,28 @@ int B = 55;
 
 // Returns 0-255 value from a x,y point in space
 // The operation will happen until it diverges (abs bigger than 2) or too many operations happen
-// If too many operations happen, nb_iter > threshold, it converged, giving us a 0, blackness (?)
+// If too many operations happen, nb_iter > threshold, it converged, giving us a 0, blackness
 // If it diverges before the threshold,  it scales then amount of color to how long it took to diverge
 // That color scaling is just to make it look prettier.
 int value (int x, int y)  {
   // scale and translate point
-  complex<float> point((float)x/WIDTH-1.5, (float)y/HEIGHT-0.5);
+  // x L/R, y U/D
+  complex<float> point((float)x/width-x_base_translation, (float)y/height-y_base_translation);
   complex<float> z(0, 0);
   int nb_iter = 0;
-  while (abs (z) < 2 && nb_iter <= ITER_THRESHOLD) {
+  while (abs (z) < 2 && nb_iter <= iter_threshold) {
     z = z * z + point;
     nb_iter++;
   }
-  if (nb_iter < ITER_THRESHOLD)
-    return (255*nb_iter)/ITER_THRESHOLD;
+  if (nb_iter < iter_threshold)
+    return (255*nb_iter)/iter_threshold;
   else
     return 0;
 }
 
 //
 std::string partial_file_name (int x) {
-  return "mandelbrot" + to_string(x / (WIDTH / THREAD_AMOUNT)) + ".not_ppm";
+  return "mandelbrot" + to_string(x / (width / thread_number)) + ".not_ppm";
 }
 
 void write_partial_file (const std::string& file_name, int starting_x, int x_amount) {
@@ -58,7 +64,7 @@ void write_partial_file (const std::string& file_name, int starting_x, int x_amo
     int j;
     int value_here;
     for (i = starting_x; i < starting_x + x_amount - 1; i++) {
-      for (j = 0; j < HEIGHT; j++) {
+      for (j = 0; j < height; j++) {
         value_here = value(i, j);
         partial_mandelbrot_file << value_here << ' ' << RGB::G << ' ' << int(RGB::B + value_here / 3) << "\n";
 //        if (j == HEIGHT / 2 && i % 100 == 0) {
@@ -68,32 +74,20 @@ void write_partial_file (const std::string& file_name, int starting_x, int x_amo
     }
   }
   partial_mandelbrot_file.close();
-  cout << "file " << file_name << "finished" << endl;
+  cout << "file " << file_name << " finished" << endl;
 }
 
 #if 0
 int main ()  {
 
   auto start = high_resolution_clock::now();
-
-// ppm format
-//  P3
-//  3 2
-//  255
-//  # The part above is the header
-//  # "P3" means this is a RGB color image in ASCII
-//  # "3 2" is the width and height of the image in pixels
-//  # "255" is the maximum value for each color
-//  # The part below is image data: RGB triplets
-//  255   0   0     0 255   0     0   0 255
-//  255 255   0   255 255 255     0   0   0
   ofstream my_Image ("mandelbrot.ppm");
 
-  // For earch point, get its value and write it to the image
+  // For each point, get its value and write it to the image
   if (my_Image.is_open ()) {
-    my_Image << "P3\n" << WIDTH << " " << HEIGHT << " 255\n";
-    for (int i = 0; i < WIDTH; i++) {
-      for (int j = 0; j < HEIGHT; j++)  {
+    my_Image << "P3\n" << width << " " << height << " 255\n";
+    for (int i = 0; i < width; i++) {
+      for (int j = 0; j < height; j++)  {
         int val = value(i, j);
         my_Image << val << ' ' << 0 << ' ' << 0 << "\n";
       }
@@ -103,9 +97,6 @@ int main ()  {
   else
     cout << "Could not open the file";
   auto stop = high_resolution_clock::now();
-  // Get duration. Substart timepoints to
-  // get durarion. To cast it to proper unit
-  // use duration cast method
   auto duration_microseconds = duration_cast<microseconds>(stop - start);
   auto duration_seconds = duration_cast<seconds>(stop - start);
 
@@ -118,21 +109,52 @@ int main ()  {
   return 0;
 }
 #else
+
+class mandelbrot_partial_thread_obj {
+ public:
+  void operator()(int starting_x, int x_amount, int thread_id, const std::string& file_name) {
+    ofstream partial_mandelbrot_file(file_name);
+    if (partial_mandelbrot_file.is_open()) {
+      int i;
+      int j;
+      int value_here;
+      for (i = starting_x; i < starting_x + x_amount - 1; i++) {
+        for (j = 0; j < height; j++) {
+          value_here = value(i,j);
+          partial_mandelbrot_file << value_here << ' ' << RGB::G << ' ' << int(RGB::B + value_here/3) << "\n";
+          if (j == height / 2 && i % 200 == 0) {
+            cout << "thread " << thread_id << " in step " << i << " of " << starting_x + x_amount << endl;
+          }
+        }
+      }
+      partial_mandelbrot_file.close();
+    } else cout << "Could not open the file " << file_name << endl;
+    cout << "Thread " << thread_id << " finished" << endl;
+  }
+};
+
 int main() {
   auto start = high_resolution_clock::now();
   std::vector<std::tuple<int, std::string>> thread_start_data;
 
-  int x_slice_size = WIDTH / THREAD_AMOUNT;
+  int x_slice_size = height / thread_number;
   int temp_size = 0;
-  for (int i = 0; i < THREAD_AMOUNT; i++) {
+  for (int i = 0; i < thread_number; i++) {
     // stores the start value for the calculation to be made
     thread_start_data.emplace_back(std::make_tuple(temp_size, partial_file_name(temp_size)));
     temp_size += x_slice_size;
     cout << to_string(temp_size) << endl;
   }
 
-  for (auto &[size, file_name]: thread_start_data){
-    write_partial_file(file_name, size, x_slice_size);
+  std::vector<std::thread> thread_array;
+  int temp_thread_id = 0;
+  for (auto &[starting_point, file_name]: thread_start_data) {
+    thread_array.emplace_back(thread(mandelbrot_partial_thread_obj(),
+                                     starting_point,
+                                     x_slice_size,
+                                     temp_thread_id,
+                                     file_name));
+    temp_thread_id++;
   }
 
   std::default_random_engine e((unsigned int) time(0));
@@ -140,9 +162,10 @@ int main() {
   std::string file_name("mandelbrot_final" + to_string(int_suffix) + ".ppm");
   ofstream my_Image(file_name);
   if (my_Image.is_open()) {
-    my_Image << "P3\n" << WIDTH << " " << HEIGHT << " 255\n";
+    my_Image << "P3\n" << width << " " << height << " 255\n";
     std::vector<std::ifstream> input_files;
-    for (int i = 0; i < THREAD_AMOUNT; i++) {
+    for (int i = 0; i < thread_number; i++) {
+      thread_array[i].join();
       auto temp_file = std::ifstream(std::get<1>(thread_start_data[i]), std::ios_base::binary);
       my_Image << temp_file.rdbuf();
     }
@@ -150,9 +173,6 @@ int main() {
   my_Image.close();
 
   auto stop = high_resolution_clock::now();
-  // Get duration. Substart timepoints to
-  // get durarion. To cast it to proper unit
-  // use duration cast method
   auto duration_microseconds = duration_cast<microseconds>(stop - start);
   auto duration_seconds = duration_cast<seconds>(stop - start);
 
